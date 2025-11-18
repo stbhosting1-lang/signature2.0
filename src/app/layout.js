13507139f5f41491943b2070ed5ef99ddd8bcac1
umayrrnaqvi <umairnaqvi902@gmail.com>
2025-11-19 01:20:44 +0500

    adds favicon.ico

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,47 +28,48 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Sorts+Mill+Goudy:wght@400;700&family=Outfit:wght@400;500;600&family=Figtree:wght@400;500;600&display=swap"
           rel="stylesheet"
         />
-
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-              <link rel="manifest" href="/site.webmanifest"/>
-
-
-              <script
-                type="application/ld+json"
-                dangerouslySetInnerHTML={{
-                  __html: JSON.stringify({
-                    "@context": "https://schema.org",
-                    "@type": "Restaurant",
-                    "name": "Signature Restaurant",
-                    "image": "https://signature.punjabland.ca/logo.jpg",  // Logo for Google
-                    "logo": "https://signature.punjabland.ca/logo.jpg",   // Very important
-                    "address": {
-                      "@type": "PostalAddress",
-                      "streetAddress": "378 Queen St E",
-                      "addressLocality": "Brampton",
-                      "addressRegion": "ON",
-                      "postalCode": "L6V 1C3",
-                      "addressCountry": "CA"
-                    },
-                    "telephone": "+1 905-459-5559",
-                    "url": "https://signature.punjabland.ca",
-                    "servesCuisine": "Indian",
-                    "menu": "https://signature.punjabland.ca/menu",
-                    "priceRange": "$$",
-                    "openingHours": ["Mo-Su 16:00-02:00"]
-                  })
-                }}
-              />
+       
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
 
 
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Restaurant",
+              "name": "Signature Restaurant",
+              "image": "https://signature.punjabland.ca/logo.jpg",  // Logo for Google
+              "logo": "https://signature.punjabland.ca/logo.jpg",   // Very important
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "378 Queen St E",
+                "addressLocality": "Brampton",
+                "addressRegion": "ON",
+                "postalCode": "L6V 1C3",
+                "addressCountry": "CA"
+              },
+              "telephone": "+1 905-459-5559",
+              "url": "https://signature.punjabland.ca",
+              "servesCuisine": "Indian",
+              "menu": "https://signature.punjabland.ca/menu",
+              "priceRange": "$$",
+              "openingHours": ["Mo-Su 16:00-02:00"]
+            })
+          }}
+        />
 
-            </head>
-            <body className="font-body">
-              <Navbar />
-              {children}
-            </body>
-          </html>
-          );
+
+
+      </head>
+      <body className="font-body">
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
 }
