ce95479ae4b6d4d0c5b421ef420f62f1a6c02890
umayrrnaqvi <umairnaqvi902@gmail.com>
2025-11-27 15:30:25 +0500

    add a button in drinks menu to explore food menu

--- src/app/drinks-menu/page.jsx
+++ src/app/drinks-menu/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import heroImg from "../../../public/drinkmenuhero.jpg"
 import Drinks from "./Drinks"
-
+import Link from 'next/link'
 export const metadata = {
     title: "Drinks Menu | Signature Restaurant",
     description: "Explore the exclusive drinks menu at Signature Restaurant, featuring handcrafted cocktails, premium beverages, and refreshing non-alcoholic options. Perfectly paired with our culinary delights for an unforgettable dining experience in Brampton.",
@@ -37,6 +37,15 @@ const page = () => {
 
                     {/* Right Menu Section */}
                     <div className="xl:ml-[560px] 2xl:ml-[1100px] mt-10 xl:mt-0 px-4 xl:px-6">
+
+                        <div className='w-full max-w-[700px] border-aboutCardBorder border bg-aboutCardBorder/5 p-4 rounded-xl flex flex-col lg:flex-row justify-between gap-5 lg:items-center mb-5'>
+                            <h3 className='text-[24px] font-heading italic text-aboutCardHeading'>Explore Our Food menu</h3>
+                            <Link href="/menu" className="block">
+                                <button className='bg-background px-4 py-2 border-aboutCardHeading border rounded-lg w-[150px]'>
+                                    <p className='text-[16px] text-aboutCardHeading font-body'>Food Menu</p>
+                                </button>
+                            </Link>
+                        </div>
                         <Drinks />
                     </div>
 
